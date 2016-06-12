6a5977d01c6b7a6b9bfb8262c89d51de7463ba89
Walter Cardoso dos Santos Filho <wcsantosfilho@gmail.com>
2016-06-12 12:21:59 -0300

    Reforma do Index.php e Inclusao das funcoes de conteudo. BD também reformado

---
+++ conteudoHTML.php
@@ -0,0 +1,219 @@
+<?php
+
+function geraPaginaConteudo($sqlresult)
+{
+    if (isset($sqlresult)) {
+        echo '<div class="container-fluid">';
+        echo '<div class="row">';
+        echo '<div class="col-md-2">';
+        echo '</div>';
+        echo '<div class="col-md-8">';
+        echo '<div class="jumbotron">';
+
+        foreach ($sqlresult as $linhas) {
+            $tagI = $linhas['taginicial'];
+            $tagF = $linhas['tagfinal'];
+            $linhaHT = $linhas['linhaHTML'];
+            switch ($tagI) {
+                case "data":
+                    if (count($tagI) > 0 and !empty($tagI)) {
+                        echo "<$tagI>";
+                    }
+                    $varData = $linhaHT;
+                    echo date($varData);
+                    if (count($tagF) > 0 and !empty($tagF)) {
+                        echo "<$tagF>";
+                    }
+                    break;
+                default:
+                    if (count($tagI) > 0 and !empty($tagI)) {
+                        echo "<$tagI>";
+                    }
+                    if (count($linhaHT) > 0 and !empty($linhaHT)) {
+                        echo $linhaHT;
+                    }
+                    if (count($tagF) > 0 and !empty($tagF)) {
+                        echo "<$tagF>";
+                    }
+                    break;
+            }
+        }
+
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="col-md-2">';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+    }
+}
+
+function geraFormulario($form_acao, $form_botoes, $form_texto, $form_campos)
+{
+    echo '<div class="container-fluid">';
+    echo '<div class="row">';
+    echo '<div class="col-md-4">';
+    if (isset($form_texto)) {
+        echo '<blockquote>';
+        echo '<p>' . $form_texto['form_action'] . '</p>';
+        echo '</blockquote>';
+    } else {
+        echo '<p> Por favor, preencha o formulário! </p>';
+    }
+    echo '</div>';
+
+    if (isset($form_acao)) {
+        echo '<div class="col-md-8">';
+        echo '<div class="jumbotron">';
+        printf('<form action="/%s" method="get">', $form_acao['form_action']);
+    } else {
+        echo '<p> Erro na configuração do Formulário </p>';
+    }
+
+    if (isset($form_campos)) {
+        foreach ($form_campos as $key => $value) {
+            echo '<div class="form-group">';
+            printf('<label class="control-label" for="%s">%s</label>', $form_campos[$key]['form_campo'], $form_campos[$key]['form_label']);
+            printf('<input class="form-control" id="%s" name="%s" type="%s"/>', $form_campos[$key]['form_campo'], $form_campos[$key]['form_campo'], $form_campos[$key]['form_tipo']);
+            echo '</div>';
+        }
+    } else {
+        echo '<p> Formulário não tem campos definidos!</p>';
+    }
+    if (isset($form_botoes)) {
+        foreach ($form_botoes as $key => $value) {
+            echo '<div class="form-group">';
+            printf('<button class="btn btn-primary" name="%s" type ="%s">%s</button>', htmlspecialchars($form_botoes[$key]['form_campo']), $form_botoes[$key]['form_campo'], $form_botoes[$key]['form_label']);
+            echo '</div>';
+        }
+    } else {
+        echo '<p> Formulário não tem campos definidos!</p>';
+    }
+    if (isset($form_acao)) {    // Fecha o FORM ao fim do formulário, caso tenha sido criado lá em cima
+        echo '</form>';
+        echo '</div>';
+        echo '</div>';
+    }
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+
+}
+
+
+function geraRodape($sqlresult)
+{
+    if (isset($sqlresult)) {
+        echo '<div class="container-fluid">';
+        echo '<div class="row-fluid">';
+
+        foreach ($sqlresult as $linhas) {
+            $tagI = $linhas['taginicial'];
+            $tagF = $linhas['tagfinal'];
+            $linhaHT = $linhas['linhaHTML'];
+            switch ($tagI) {
+                case "data":
+                    if (count($tagI) > 0 and !empty($tagI)) {
+                        echo "<$tagI>";
+                    }
+                    $varData = $linhaHT;
+                    echo date($varData);
+                    if (count($tagF) > 0 and !empty($tagF)) {
+                        echo "<$tagF>";
+                    }
+                    break;
+                default:
+                    if (count($tagI) > 0 and !empty($tagI)) {
+                        echo "<$tagI>";
+                    }
+                    if (count($linhaHT) > 0 and !empty($linhaHT)) {
+                        echo $linhaHT;
+                    }
+                    if (count($tagF) > 0 and !empty($tagF)) {
+                        echo "<$tagF>";
+                    }
+            }
+        }
+        echo '</div>';
+        echo '</div>';
+    }
+}
+
+function geraResposta($responseGet)
+{
+    if (isset($responseGet)) {
+        echo '<div class="container-fluid">';
+        echo '<div class="row">';
+        echo '<div class="col-md-2">';
+        echo '</div>';
+        echo '<div class="col-md-8">';
+        echo '<div class="jumbotron">';
+        echo '<p>Recebemos com exito as seguintes informações. Seus dados serão analisados e responderemos em até 5 dias úteis</p>';
+        echo '<table class="table">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th>Campo</th>';
+        echo '<th>Dado</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        foreach ($responseGet as $key => $value) {
+            if (!empty($key) and !empty($value)) {
+                echo '<tr>';
+                printf('<td>%s</td><td>%s</td>', $key, $value);
+                echo '</tr>';
+            }
+        }
+        echo '</tbody>';
+        echo '</table>';
+        echo '<a href="/home">Voltar para Home</a>';
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="col-md-2">';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+
+    } else {
+        echo '<p> Nenhum dado recebido. Avalie o preenchimento do formulario.</p>';
+    }
+
+}
+
+function geraResultadoBusca ($sqlresult, $textobusca) {
+    if (empty($sqlresult)) {
+        echo '<div class="panel panel-default">';
+        echo '<div class="panel-heading">Não foram encontrados resultados para: <b>' . $textobusca . '</b>.</div>';
+    } else {
+        echo '<div class="container-fluid">';
+        echo '<div class="row">';
+        echo '<div class="col-md-2">';
+        echo '</div>';
+        echo '<div class="panel panel-default">';
+        echo '<div class="panel-heading">Resultado da Pesquisa</div>';
+        echo '<table class="table">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th>Página</th>';
+        echo '<th>Linha</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+
+        foreach ($sqlresult as $searchresult) {
+            echo '<tr>';
+            echo '<td><li><a href="' . $searchresult['rotas_arquivo'] . '">' . $searchresult['rotas_arquivo'] . '</a></td><td>' . $searchresult['linhaHTML'] . '</td></li>';
+            echo '</tr>';
+        }
+        echo '</tbody>';
+        echo '</table>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="col-md-2">';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+    }
+
+}
